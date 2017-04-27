809a7c566b0de19830366a0eed751a615c34802f
Galen Weber <galenweber@gmail.com>
2017-04-27 18:20:43 -0400

    adds google analytics tracking code

--- server-app/bundle.js
+++ server-app/bundle.js
@@ -31869,6 +31869,9 @@ function Html(props) {
         name: 'viewport',
         content: 'width=device-width, initial-scale=1'
       }),
+      _react2['default'].createElement('script', {
+        dangerouslySetInnerHTML: { __html: '\n            (function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){\n            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n            })(window,document,\'script\',\'https://www.google-analytics.com/analytics.js\',\'ga\');\n\n            ga(\'create\', \'UA-98252156-1\', \'auto\');\n            ga(\'send\', \'pageview\');\n          ' }
+      }),
       _react2['default'].createElement('link', {
         rel: 'stylesheet',
         type: 'text/css',
