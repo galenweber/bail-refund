4dff83e6f11558dd351c9dd27d663e62b6e64a85
Galen Weber <galenweber@gmail.com>
2017-04-30 12:31:34 -0400

    adds analytics tracking

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,21 +10,31 @@ import TermsBanner from '../../components/TermsBanner';
 import LivePayout from '../../components/LivePayout';
 import Footer from '../../components/Footer';
 
-export default function Home() {
-  return (
-    <div>
-      <Helmet>
-        <title>NYC Bail Refund Check | We Pay Cash For Bail Money Receipt</title>
-        <meta name="description" content="Don't wait 8 weeks. We pay cash for NYC bail refund receipts. Apply in minutes and receive up to $5,000 the same day. Over 98% of applicants approved." />
-      </Helmet>
-      <Navbar />
-      <CallToAction />
-      <HowWorks />
-      <LivePayout />
-      <ApplyNow />
-      <FaqBanner />
-      <TermsBanner />
-      <Footer />
-    </div>
-  )
+export default class Home extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {src: ''};
+  }
+
+  render() {
+    const { src } = this.state;
+    return (
+      <div>
+        <Helmet>
+          <title>NYC Bail Refund Check | We Pay Cash For Bail Money Receipt</title>
+          <meta name="description" content="Don't wait 8 weeks. We pay cash for NYC bail refund receipts. Apply in minutes and receive up to $5,000 the same day. Over 98% of applicants approved." />
+        </Helmet>
+        <Navbar />
+        <CallToAction />
+        <input type="file" accept="image/*" capture="camera" id="camera" />
+        <img src={src} id="frame" />
+        <HowWorks />
+        <LivePayout />
+        <ApplyNow />
+        <FaqBanner />
+        <TermsBanner />
+        <Footer />
+      </div>
+    )
+  }
 }
